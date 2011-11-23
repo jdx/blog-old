ef51e98722cf219305ee2020f8187b980fab0911
Jeff Dickey <dickeytk@gmail.com>
2011-11-22 17:42:39 -0800

    fixed migration

--- db/migrate/20111123003613_db_update.rb
+++ db/migrate/20111123003613_db_update.rb
@@ -1,7 +1,7 @@
 class DbUpdate < ActiveRecord::Migration
   def change
     add_column :posts, :description, :text
-    add_column :posts, :thumbnail, :url
+    add_column :posts, :thumbnail, :string
     add_column :posts, :datetime, :datetime
     add_column :posts, :draft, :boolean
     add_index :posts, :datetime
