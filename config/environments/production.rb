1f2d8eb96d3321a6b8aa13c47a5678b883c8aaed
Jeff Dickey <dickeytk@gmail.com>
2011-11-30 18:40:43 -0800

    removed cloudfront

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,9 +40,6 @@ Blog::Application.configure do
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://assets.jeffdickey.info"
-
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
