a8982e68a183c51daf3aac12c5cfc54919d2370c
Jeff Dickey <me@jeffdickey.info>
2012-02-24 12:40:04 -0800

    changed dns

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Blog::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.host = 'jeffdickey.info'
+  config.host = 'dickey.xxx'
 
   # Code is not reloaded between requests
   config.cache_classes = true
