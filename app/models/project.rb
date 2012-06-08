1f28ae106c38190bac275bf3e5439f53999612dc
Jeff Dickey <jeff@dickey.xxx>
2012-06-07 21:25:36 -0700

    fixed project pages

--- app/models/project.rb
+++ app/models/project.rb
@@ -14,20 +14,4 @@ class Project < ActiveRecord::Base
   def to_param
     "#{ self.id }-#{ self.slug }"
   end
-
-  def body_html
-    body_html = Rails.cache.fetch "#{cache_key}/body_html" do
-      options = [:hard_wrap, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
-      syntax_highlighter(Redcarpet.new(body, *options).to_html)
-    end
-    return body_html.html_safe
-  end
-
-  def syntax_highlighter(text)
-    text = text.gsub(/\<pre( lang="(.+?)")?\>\<code\>(.+?)\<\/code\><\/pre\>/m) do
-      code = HTMLEntities.new.decode $3
-      CodeRay.scan(code, $2).div(:css => :class)
-    end
-  end
-
 end
