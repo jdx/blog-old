d621cca1ffa1b546d70dc71ebb8bd79b26aec6e1
dickeyxxx <jeff@dickey.xxx>
2013-01-08 13:32:46 -0800

    fixed carrerwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider => "AWS",
-    :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    provider: "AWS",
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    hosts: "http://#{ENV['S3_BUCKET']}.s3.amazonaws.com",
   }
   config.fog_directory = ENV['S3_BUCKET']
-  config.fog_host = "http://#{ENV['S3_BUCKET']}.s3.amazonaws.com"
   if Rails.env.production?
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
     config.fog_host = 'http://content.jeffdickey.info'
