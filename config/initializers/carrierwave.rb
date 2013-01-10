a0285165a68967025da0bbbc4b856ceaa5d5d0df
dickeyxxx <jeff@dickey.xxx>
2013-01-10 15:12:48 -0800

    fixed assets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,12 @@ CarrierWave.configure do |config|
     provider: "AWS",
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    hosts: Rails.env.production? ?  'http://content.jeffdickey.info': "http://#{ENV['S3_BUCKET']}.s3.amazonaws.com",
   }
   config.fog_directory = ENV['S3_BUCKET']
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} if Rails.env.production?
+  if Rails.env.production?
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+    config.asset_host = 'http://content.jeffdickey.info'
+  end
 end
 if Rails.env.test? or Rails.env.cucumber?
   CarrierWave.configure do |config|
